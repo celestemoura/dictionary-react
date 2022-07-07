0d4edf21c0b5721e2c7994ee99809cb71f04c791
Celeste Moura <37707126+celestemoura@users.noreply.github.com>
2022-07-07 14:58:40 +0200

    rendered definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [query, setQuery] = useState(null);
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -30,6 +32,7 @@ export default function Dictionary() {
           onClick={search}
         ></i>
       </form>
+      <Results results={results} />
     </div>
   );
 }
