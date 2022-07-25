6597f1a7dd3a22fdc5972e7f38e17c92b5c69458
Celeste Moura <37707126+celestemoura@users.noreply.github.com>
2022-07-25 16:22:21 +0200

    added image API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Photos from "./Photos";
 import Results from "./Results";
 import "./Dictionary.css";
 
@@ -7,19 +8,26 @@ export default function Dictionary(props) {
   const [query, setQuery] = useState(props.defaultQuery);
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-      setResults(response.data[0]);
+    setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     //https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
-    if (query === null) {
-      alert("no");
-    } else {
-      axios.get(apiUrl).then(handleResponse);
-    }
+    axios.get(apiUrl).then(handleResponse);
+
+    const pexelsApiKey =
+      "563492ad6f91700001000001d1dc787f1d31428abfcb8e2e30356da0";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${query}&per_page=12`;
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function load() {
@@ -57,6 +65,7 @@ export default function Dictionary(props) {
           </div>
         </div>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4 each-img" key={index}>
+                <a href={photo.url} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    alt={props.photos.alt}
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+        <div className="photo-source-info">
+          Photos provided by{" "}
+          <a
+            href="https://www.pexels.com/"
+            target="_blank"
+            rel="noreferrer"
+            title="Pexels"
+          >
+            Pexels
+          </a>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
